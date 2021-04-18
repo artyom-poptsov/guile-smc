a3d160e249df09cb140207f067e866064cbc6a09
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-04-18 23:09:30 +0300

    examples/puml.scm: Update

--- examples/puml.scm
+++ examples/puml.scm
@@ -2,7 +2,15 @@
 -L modules -e main -s
 !#
 
-(use-modules (smc puml))
+(use-modules (ice-9 pretty-print)
+             (smc puml)
+             (smc fsm))
+
+
 
 (define (main args)
-  (format #t "result: ~a~%" (puml->fsm (current-input-port))))
+  (let ((fsm (puml->fsm (current-input-port))))
+    (format #t "output fsm: ~a~%" fsm)
+    (format #t "transition table:~%")
+    (pretty-print
+     (hash-table->transition-list (fsm-transition-table fsm)))))
