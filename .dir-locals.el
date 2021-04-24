4665705571dcefe22ae037d5906e7b3581c50e6c
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-04-24 20:08:01 +0300

    .dir-locals.el: Add to the repository

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;;; .dir-locals.el -- Per-directory local variables for GNU Emacs 23 and later.
+
+((nil         . ((fill-column  . 78)
+                 (tab-width    .  8)))
+ (scheme-mode . ((indent-tabs-mode . nil)
+                 (eval . (put 'test-assert 'scheme-indent-function 1))
+                 (eval . (put 'test-error 'scheme-indent-function 1))
+                 (eval . (put 'test-equal 'scheme-indent-function 1))
+                 (eval . (put 'test-assert-with-log 'scheme-indent-function 1)))))
+
+;;; .dir-locals.el ends here
+
