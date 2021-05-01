41bcb56c0ca82ebe5fb5ccb3588b4b168f0875da
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-05-01 12:29:44 +0300

    modules/smc/core/log.scm: Add comments

--- modules/smc/core/log.scm
+++ modules/smc/core/log.scm
@@ -1,3 +1,28 @@
+;;; log.scm -- Guile-SMC logging facilities.
+
+;; Copyright (C) 2021 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains Guile-SMC logging facilities.
+
+
+;;; Code:
+
 (define-module (smc core log)
   #:use-module (oop goops)
   #:export (log
@@ -13,6 +38,7 @@
 (define *use-stderr?* #f)
 
 
+;; Enable or disable logging to stderr.
 (define-method (log-use-stderr! (value <boolean>))
   (set! *use-stderr?* value))
 
@@ -31,14 +57,19 @@
       (error "Could not log a message"))))
 
 (define (log-error fmt . args)
+  "Log a formatted error message."
   (apply log "err" fmt args))
 
 (define (log-warning fmt . args)
+  "Log a formatted warning message."
   (apply log "warning" fmt args))
 
 (define (log-info fmt . args)
+  "Log a formatted informational message."
   (apply log "info" fmt args))
 
 (define (log-debug fmt . args)
+  "Log a formatted debug message."
   (apply log "debug" fmt args))
 
+;;; log.scm ends here.
