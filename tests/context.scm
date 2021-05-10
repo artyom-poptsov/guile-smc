5a3dd499f9ebd8cfed8d1d118e15a627022ed3d9
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-05-11 00:07:25 +0300

    tests/context.scm: Improve

--- tests/context.scm
+++ tests/context.scm
@@ -1,12 +1,17 @@
 (use-modules (srfi srfi-64)
              (srfi srfi-26)
              (oop goops)
+             ((smc core stack)
+              #:prefix smc:)
              (smc context char-context))
 
+
 (define %test-suite-name "context")
 
-
 (test-begin %test-suite-name)
+
+
+;;; Guards.
 
 (test-assert "guard:#t"
   (guard:#t 'event 'context))
@@ -34,6 +39,34 @@
 
 (test-assert "guard:at-symbol?"
   (guard:at-symbol? #\@ '()))
+
+
+;;; Generic context.
+
+(test-assert "context?"
+  (and (context? (make <context>))
+       (not (context? "not a context"))))
+
+(test-assert "<context>: buffer and stanza initially must be empty stacks"
+  (let ((ctx (make <context>)))
+    (and (smc:stack?       (context-buffer ctx))
+         (smc:stack-empty? (context-buffer ctx))
+         (smc:stack?       (context-stanza ctx))
+         (smc:stack-empty? (context-stanza ctx)))))
+
+(test-equal "action:store"
+  '("world" "hello")
+  (let ((ctx (make <context>)))
+    (action:store "world" (action:store "hello" ctx))
+    (smc:stack-content (context-buffer ctx))))
+
+(test-assert "action:update-stanza"
+  (let ((ctx (make <context>)))
+    (action:store "world" (action:store "hello" ctx))
+    (action:update-stanza #f ctx)
+    (and (equal? (smc:stack-content (context-stanza ctx))
+                 '(("hello" "world")))
+         (smc:stack-empty? (context-buffer ctx)))))
 
 
 (define exit-status (test-runner-fail-count (test-runner-current)))
