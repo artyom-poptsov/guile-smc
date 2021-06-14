b65b0323bf67c8df20f34d267d56d1b047ed0687
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-06-14 22:58:13 +0300

    guix.scm: Update

--- guix.scm
+++ guix.scm
@@ -1,3 +1,37 @@
+;; guix.scm --- GNU Guix package recipe    -*- coding: utf-8 -*-
+;;
+;; Copyright (C) 2021 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This file is part of Guile-SMC.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;;
+;; GNU Guix development package. To use as the basis for a development
+;; environment, run:
+;;
+;;  guix environment --pure --container -l guix.scm
+;;
+;; In the new shell, run:
+;;
+;;  autoreconf -vif && ./configure && make check
+;;
+;;; Code:
+
+
 (use-modules (guix packages)
              (guix licenses)
              (guix git-download)
