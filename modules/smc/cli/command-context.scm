d15cbb9ebe912dface3cb60c017db1a083548d4a
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-07-24 17:41:06 +0300

    modules/smc/cli/command-context: Implement context genereation
    * modules/smc/cli/command-context.scm (command-context): Add new options:
      '--generate' and '--generate-module'.
      (%write-separator, %write-commentary, %generate-context-module)
      (%generate-context-action-procedure)
      (%generate-context-guard-procedure)
      (generate-context): New procedures.

--- modules/smc/cli/command-context.scm
+++ modules/smc/cli/command-context.scm
@@ -2,6 +2,7 @@
   #:use-module (ice-9 getopt-long)
   #:use-module (ice-9 pretty-print)
   #:use-module (smc core log)
+  #:use-module (smc core set)
   #:use-module (smc compiler)
   #:use-module (smc fsm)
   #:use-module (smc puml)
@@ -18,10 +19,69 @@ Options:
   --help, -h        Print this message and exit.
   --resolve, -r     Show resolved and unresolved procedures.
   --generate, -g    Generate a context stub from a given PlantUML file.
-                    (Not implemented yet.)
+  --generate-module, -G <module>
+                    Generate a context module stub from a given PlantUML file.
   --debug           Enable the debug mode.
 
 "))
+
+
+
+(define (%write-separator port)
+  (display #\ff port)
+  (newline port))
+
+(define %write-commentary
+  (case-lambda
+    ((port comment)
+     (format port ";; ~a~%" comment))
+    ((port . comments)
+     (for-each (lambda (line)
+                 (format port ";; ~a~%" line))
+               comments))))
+
+(define (%generate-context-module module exports port)
+  (pretty-print `(define-module ,(eval-string
+                                  (string-append "(quote " module ")"))
+                   #:export ,exports)
+                port)
+  (newline port))
+
+(define (%generate-context-action-procedure name port)
+  (pretty-print `(define (,name ctx event)
+                   ctx)
+                port)
+  (newline port))
+
+(define (%generate-context-guard-procedure name port)
+  (pretty-print `(define (,name ctx event)
+                   #t)
+                port)
+  (newline port))
+
+(define (generate-context module procedures port)
+  (when module
+    (%generate-context-module module procedures port))
+
+  (newline port)
+  (%write-separator port)
+  (%write-commentary port "Guards.")
+  (for-each (lambda (proc)
+              (%generate-context-guard-procedure proc port))
+            (filter (lambda (proc)
+                      (string-prefix? "guard:" (symbol->string proc)))
+                    procedures))
+
+  (newline port)
+  (%write-separator port)
+  (%write-commentary port "Actions.")
+  (for-each (lambda (proc)
+              (%generate-context-action-procedure proc port))
+            (filter (lambda (proc)
+                      (string-prefix? "action:" (symbol->string proc)))
+                    procedures)))
+
+
 
 
 (define %option-spec
@@ -29,7 +89,8 @@ Options:
     (load-path                (single-char #\L) (value #t))
     (modules                  (single-char #\U) (value #t))
     (resolve                  (single-char #\r) (value #f))
-    ;; (generate                 (single-char #\g) (value #f))
+    (generate                 (single-char #\g) (value #f))
+    (generate-module          (single-char #\G) (value #t))
     (debug                                      (value #f))))
 
 (define (command-context args)
@@ -38,6 +99,7 @@ Options:
          (modules          (option-ref options 'modules   #f))
          (resolve?         (option-ref options 'resolve   #f))
          (generate?        (option-ref options 'generate  #f))
+         (generate-module  (option-ref options 'generate-module #f))
          (debug-mode?      (option-ref options 'debug     #f)))
 
     (when (option-ref options 'help #f)
@@ -55,7 +117,25 @@ Options:
                             #:keep-going? #t
                             #:debug-mode? debug-mode?)))
         (puml-context-print-resolver-status (fsm-parent-context fsm)
-                                            (current-output-port)))))))
+                                            (current-output-port))))
+     (generate?
+      (let ((fsm (puml->fsm (current-input-port)
+                            #:module      (puml-modules modules)
+                            #:keep-going? #t
+                            #:debug-mode? debug-mode?)))
+        (generate-context #f
+                          (set-content (puml-context-unresolved-procedures
+                                        (fsm-parent-context fsm)))
+                          (current-output-port))))
+     (generate-module
+      (let ((fsm (puml->fsm (current-input-port)
+                            #:module      (puml-modules modules)
+                            #:keep-going? #t
+                            #:debug-mode? debug-mode?)))
+        (generate-context generate-module
+                          (set-content (puml-context-unresolved-procedures
+                                        (fsm-parent-context fsm)))
+                          (current-output-port)))))))
 
 ;;; command-context.scm ends here.
 
