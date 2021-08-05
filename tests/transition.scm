10ac3755f3bead674cbcf4213610575ca70c1da1
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-08-05 08:24:04 +0300

    tests/transition.scm: New test suite
    * tests/transition.scm: New test suite.
    * tests/Makefile.am: Update.

---
+++ tests/transition.scm
@@ -0,0 +1,78 @@
+(use-modules (srfi srfi-64)
+             (srfi srfi-26)
+             (ice-9 receive)
+             (ice-9 regex)
+             (oop goops)
+             (smc context char-context)
+             (smc core transition))
+
+
+(define %test-name "transition")
+
+(test-begin %test-name)
+
+
+
+(test-equal "transition:guard"
+  guard:#t
+  (transition:guard `(,guard:#t ,action:no-op 'next-state)))
+
+(test-equal "transition:action"
+  action:no-op
+  (transition:action `(,guard:#t ,action:no-op 'next-state)))
+
+(test-equal "transition:next-state"
+  'next-state
+  (transition:next-state `(,guard:#t ,action:no-op next-state)))
+
+
+
+(test-equal "transition-table-append"
+  `((,guard:#t ,action:no-op next-state))
+  (transition-table-append '() guard:#t action:no-op 'next-state))
+
+(test-equal "transition-table-count: all"
+  2
+  (transition-table-count (lambda (transition) #t)
+                          (transition-table-append
+                           (transition-table-append '()
+                                                    guard:#t
+                                                    action:no-op
+                                                    'state-1)
+                           guard:#t action:no-op 'state-2)))
+
+(test-equal "transition-table-count: only state-1"
+  1
+  (transition-table-count (lambda (transition)
+                            (equal? (transition:next-state transition)
+                                    'state-1))
+                          (transition-table-append
+                           (transition-table-append '()
+                                                    guard:#t
+                                                    action:no-op
+                                                    'state-1)
+                           guard:#t action:no-op 'state-2)))
+
+
+
+(define (guard:null? ctx event)
+  (null? event))
+
+(define (action:append-error ctx event)
+  (cons 'error ctx))
+
+(test-equal "transition-table-run"
+  '(error)
+  (let ((table `((,(const #f)  ,action:no-op state-1)
+                 (,guard:null? ,action:append-error state-2))))
+    (receive (next-state new-context)
+        (transition-table-run table '() '())
+      new-context)))
+
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-name)
+
+(exit exit-status)
