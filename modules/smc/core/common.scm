e213910ead8217f22f8db99c74a0389b6a8129a5
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-11-22 22:44:00 +0300

    modules/smc/core/common.scm: Add module commentary

--- modules/smc/core/common.scm
+++ modules/smc/core/common.scm
@@ -1,3 +1,28 @@
+;;; log.scm -- Guile-SMC common code.
+
+;; Copyright (C) 2021 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains common Guile-SMC code.
+
+
+;;; Code:
+
 (define-module (smc core common)
   #:export (object-address/hex-string
             safe-module-ref))
@@ -10,3 +35,5 @@
 
 (define (object-address/hex-string object)
   (number->string (object-address object) 16))
+
+;;; common.scm ends here.
