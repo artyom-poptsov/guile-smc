b831ccae1a1b9618965edf4f0e91ccc2d6621af5
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-11-27 23:26:31 +0300

    modules/smc/core/log.scm: Improve

--- modules/smc/core/log.scm
+++ modules/smc/core/log.scm
@@ -29,6 +29,7 @@
   #:use-module (ice-9 popen)
   #:use-module (logging logger)
   #:use-module (oop goops)
+  #:use-module (smc core common)
   #:use-module (smc core state)
   #:export (log
             log-error
@@ -37,25 +38,39 @@
             log-debug
             log-use-stderr!))
 
-(define %logger-command "logger")
-(define %tag            "guile-smc")
-
 
 
 (define-class-with-docs <syslog> (<log-handler>)
   "This is a log handler which writes logs to the syslog."
+
+  ;; Syslog logger console command.
+  ;;
+  ;; <string>
+  (logger
+   #:init-value "logger"
+   #:getter     syslog-logger)
+
+  ;; syslog process tag that will be logged.
+  ;;
+  ;; <string>
   (tag
    #:init-keyword #:tag
    #:getter       syslog-tag)
+
+  ;; Should the logger log to stderr?
+  ;;
+  ;; <boolean>
   (use-stderr?
    #:init-value   #f
    #:init-keyword #:use-stderr?
    #:getter       syslog-use-stderr?
    #:setter       syslog-use-stderr!))
 
+
+
 (define-method (accept-log (log <syslog>) level time str)
   (let* ((command (format #f "~a ~a -p 'user.~a' -t '~a' '~a'"
-                          %logger-command
+                          (syslog-logger log)
                           (if (syslog-use-stderr? log)
                               "-s"
                               "")
@@ -68,18 +83,21 @@
 
 
 
-(define %syslog (make <syslog>
-                  #:tag %tag))
+(define-with-docs %syslog
+  "Default syslog handler instance for Guile-SMC."
+  (make <syslog> #:tag "guile-smc"))
 
-(define %logger (make <logger>))
+(define-with-docs %logger
+  "Default logger instance for Guile-SMC."
+  (make <logger>))
 
 (add-handler! %logger %syslog)
 (set-default-logger! %logger)
 (open-log! %logger)
 
 
-;; Enable or disable logging to stderr.
-(define-method (log-use-stderr! (value <boolean>))
+(define-method-with-docs (log-use-stderr! (value <boolean>))
+  "Enable or disable logging to stderr."
   (syslog-use-stderr! %syslog value))
 
 (define (log level fmt . args)
