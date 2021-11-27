46e34228e33a1d652045802557a56b82ab917b95
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-11-27 23:11:13 +0300

    .dir-locals.el: Update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; .dir-locals.el -- Per-directory local variables for GNU Emacs 23 and later.
 
 ((nil         . ((fill-column  . 78)
-                 (tab-width    .  8)))
+                 (tab-width    .  8)
+                 (sentence-end-double-space . t)))
  (scheme-mode . ((indent-tabs-mode . nil)
                  (eval . (put 'test-assert 'scheme-indent-function 1))
                  (eval . (put 'test-error 'scheme-indent-function 1))
