3c1c894f16a630b7d675db45945f91c43324a0b3
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-01-07 23:30:21 +0300

    tests/log.scm: New test suite
    * tests/log.scm: New test suite.
    * tests/Makefile.am: Update.

---
+++ tests/log.scm
@@ -0,0 +1,28 @@
+(use-modules (srfi srfi-64)
+             (srfi srfi-26)
+             (ice-9 receive)
+             (ice-9 regex)
+             (oop goops)
+             (smc core log))
+
+
+(define %test-name "log")
+(test-begin %test-name)
+
+
+
+(test-equal "%precise-log-formatter"
+  "2022-01-07 23:17:55.685611 (DEBUG): test\n"
+  (%precise-log-formatter 'DEBUG '(1641586675 . 685611) "test"))
+
+(test-equal "<port-log/us>: initialize"
+  %precise-log-formatter
+  (let ((h (make <port-log/us>)))
+    (slot-ref h 'formatter)))
+
+
+(define exit-status (test-runner-fail-count (test-runner-current)))
+
+(test-end %test-name)
+
+(exit exit-status)
