5870c8dbdacd98ac90839a861d606bdcc435b865
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-05-03 23:36:25 +0300

    modules/smc/cli/command-context.scm: Improve
    * modules/smc/cli/command-context.scm: Allow to generate standalone and
      intermediate SMC contexts from the Guile-SMC code.  Change options.
      (get-module-exports): New procedure.
      (generate-smc-context, generate-smc-context/standalone): New procedures.
      (command-context): Rename '--modules' option to '--use-modules'.  Add
      '--standalone' option.  Remove '--generate-module' option.

--- modules/smc/cli/command-context.scm
+++ modules/smc/cli/command-context.scm
@@ -1,12 +1,16 @@
 (define-module (smc cli command-context)
   #:use-module (ice-9 getopt-long)
   #:use-module (ice-9 pretty-print)
+  #:use-module (srfi srfi-1)
   #:use-module (smc core log)
   #:use-module (smc core set)
   #:use-module (smc compiler)
   #:use-module (smc fsm)
   #:use-module (smc puml)
   #:use-module (smc cli common)
+  #:use-module (smc config)
+  #:use-module (smc compiler guile-common)
+  #:use-module (smc compiler guile-standalone)
   #:export (command-context))
 
 (define (print-help)
@@ -19,9 +23,11 @@ INPUT-FILE is not provided, than the command will read the standard input.
 Options:
   --help, -h        Print this message and exit.
   --resolve, -r     Show resolved and unresolved procedures.
+  --standalone, -s
+                    Generate a standalone compiler context.
   --generate, -g    Generate a context stub from a given PlantUML file.
-  --generate-module, -G <module>
-                    Generate a context module stub from a given PlantUML file.
+  --module, -m <module>
+                    Place the output code into a specified module
   --debug           Enable the debug mode.
 
 "))
@@ -41,7 +47,7 @@ Options:
                  (format port ";; ~a~%" line))
                comments))))
 
-(define (%generate-context-module module exports port)
+(define* (%generate-context-module module exports port)
   (pretty-print `(define-module ,(eval-string
                                   (string-append "(quote " module ")"))
                    #:export ,exports)
@@ -82,25 +88,85 @@ Options:
                       (string-prefix? "action:" (symbol->string proc)))
                     procedures)))
 
+
 
+(define (get-module-exports defmod)
+  (if (equal? (car defmod) '#:export)
+      (cadr defmod)
+      (get-module-exports (cdr defmod))))
+
+(define (generate-smc-context module output-port)
+  (let* ((context-code (fsm-get-context-code %guile-smc-modules-directory
+                                             #:skip-define-module? #f))
+         (exports      (fold (lambda (sexp prev)
+                               (cond
+                                ((equal? (car sexp) 'define-module)
+                                 (append (get-module-exports sexp) prev))
+                                (else
+                                 prev)))
+                             '()
+                             context-code)))
+    (write-header output-port)
+    (newline)
+    (pretty-print `(define-module ,(eval-string
+                                    (string-append "(quote " module ")"))
+                     #:use-module (smc core common)
+                     #:use-module (smc context context)
+                     #:use-module (smc context char-context)
+                     #:use-module (smc core log)
+                     #:re-export ,exports))))
+
+(define* (generate-smc-context/standalone module
+                                          output-port
+                                          #:key
+                                          (optimize? #f))
+  (write-header output-port)
+  (newline)
+  (let* ((context-code (fsm-get-context-code %guile-smc-modules-directory
+                                             #:skip-define-module? #f))
+         (exports      (fold (lambda (sexp prev)
+                              (cond
+                               ((equal? (car sexp) 'define-module)
+                                (append (get-module-exports sexp) prev))
+                               (else
+                                prev)))
+                             '()
+                             context-code)))
+
+    (%generate-context-module module exports output-port)
+    (newline output-port)
+    (form-feed output-port)
+
+    (for-each (lambda (sexp)
+                (pretty-print sexp output-port #:display? #f)
+                (newline))
+              (if optimize?
+                  (prune-unused-definitions
+                   (prune-unused-definitions context-code (list fsm-code))
+                   (list fsm-code))
+                  context-code))
+
+    (newline output-port)))
 
 
 (define %option-spec
   '((help                     (single-char #\h) (value #f))
     (load-path                (single-char #\L) (value #t))
-    (modules                  (single-char #\U) (value #t))
+    (standalone               (single-char #\s) (value #f))
+    (use-modules              (single-char #\U) (value #t))
     (resolve                  (single-char #\r) (value #f))
     (generate                 (single-char #\g) (value #f))
-    (generate-module          (single-char #\G) (value #t))
+    (module                   (single-char #\m) (value #t))
     (debug                                      (value #f))))
 
 (define (command-context args)
   (let* ((options          (getopt-long args %option-spec))
          (extra-load-paths (option-ref options 'load-path ""))
-         (modules          (option-ref options 'modules   #f))
+         (extra-modules    (option-ref options 'use-modules #f))
+         (module           (option-ref options 'module    #f))
          (resolve?         (option-ref options 'resolve   #f))
          (generate?        (option-ref options 'generate  #f))
-         (generate-module  (option-ref options 'generate-module #f))
+         (standalone?      (option-ref options 'standalone #f))
          (debug-mode?      (option-ref options 'debug     #f))
          (args             (option-ref options '()        #f)))
 
@@ -122,29 +188,24 @@ Options:
       (cond
        (resolve?
         (let ((fsm (puml->fsm port
-                              #:module      (puml-modules modules)
+                              #:module      (puml-modules extra-modules)
                               #:keep-going? #t
                               #:debug-mode? debug-mode?)))
           (puml-context-print-resolver-status (fsm-parent-context fsm)
                                               (current-output-port))))
        (generate?
         (let ((fsm (puml->fsm port
-                              #:module      (puml-modules modules)
+                              #:module      (puml-modules extra-modules)
                               #:keep-going? #t
                               #:debug-mode? debug-mode?)))
-          (generate-context #f
+          (generate-context module
                             (set-content (puml-context-unresolved-procedures
                                           (fsm-parent-context fsm)))
                             (current-output-port))))
-       (generate-module
-        (let ((fsm (puml->fsm port
-                              #:module      (puml-modules modules)
-                              #:keep-going? #t
-                              #:debug-mode? debug-mode?)))
-          (generate-context generate-module
-                            (set-content (puml-context-unresolved-procedures
-                                          (fsm-parent-context fsm)))
-                            (current-output-port))))))))
+       (else
+        (if standalone?
+            (generate-smc-context/standalone module (current-output-port))
+            (generate-smc-context module (current-output-port))))))))
 
 ;;; command-context.scm ends here.
 
