39e814e6ae0126675fa1321c6fed98a37d127943
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-02-27 21:23:10 +0300

    modules/smc/context/common.scm: Add the module commentary

--- modules/smc/context/common.scm
+++ modules/smc/context/common.scm
@@ -1,3 +1,29 @@
+;;; common.scm -- Guile-SMC finite state machine common guards.
+
+;; Copyright (C) 2023 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This file contains common guard/action procedures that are independent of
+;; context type.
+
+
+;;; Code:
+
 (define-module (smc context common)
   #:export (guard:#t
             action:no-op))
