7ffe9c359c6e94d6745830ce743db2fe38f509c7
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-03-11 00:53:46 +0300

    smc/context/functional/generic (%make-context, make-context): Export
    * modules/smc/context/functional/generic.scm (%make-context, make-context):
      Export.

--- modules/smc/context/functional/generic.scm
+++ modules/smc/context/functional/generic.scm
@@ -1,7 +1,9 @@
 (define-module (smc context functional generic)
-  #:use-module (srfi srfi-9 gnu)
+  #:use-module (srfi srfi-9 gnqu)
   #:use-module (smc core common)
   #:export (<context>
+            %make-context
+            make-context
             context?
             context-debug-mode?
             context-debug-mode-set
