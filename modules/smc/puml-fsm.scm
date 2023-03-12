01a9f31dbdca1f33eabaa629faf6714808b8f7dc
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-03-12 22:54:28 +0300

    puml.puml: Replace "action:add-description" with "add-description"
    * modules/smc/puml.puml: Replace "action:add-description" with "add-description".
    * modules/smc/puml-fsm.scm: Update.

--- modules/smc/puml-fsm.scm
+++ modules/smc/puml-fsm.scm
@@ -8,7 +8,7 @@
 ;;;   <https://github.com/artyom-poptsov/guile-smc>
 ;;;
 ;;; Statistics:
-;;;   step-counter:              6259
+;;;   step-counter:              6252
 ;;;   transition-counter:         788
 ;;;
 ;;; Resolver status:
