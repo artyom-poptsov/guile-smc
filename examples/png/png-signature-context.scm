c9469d743504b6a67806afce13524ab93e3ebe1b
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-03-12 20:58:07 +0300

    examples/png/png-signature.puml: Update
    * examples/png/png-signature-context.scm: Update.
    * examples/png/png-signature.puml: Update.

--- examples/png/png-signature-context.scm
+++ examples/png/png-signature-context.scm
@@ -1,44 +1,24 @@
 (define-module (png-signature-context)
-  #:export (guard:correct-first-byte?
-            guard:letter-P?
-            guard:letter-N?
-            guard:letter-G?
-            guard:letter-cr?
-            guard:letter-lf?
-            guard:letter-ctrl-z?
-            guard:letter-lf?
+  #:use-module (smc context char)
+  #:re-export (char:letter-P?
+               char:letter-N?
+               char:letter-G?
+               char:cr?
+               char:lf?
+               char:sub?)               ; Ctrl+Z
+  #:export(guard:correct-first-byte?
+           action:wrong-first-byte-error
+           action:unexpected-eof-error
+           action:unexpected-byte-error))
 
-            action:wrong-first-byte-error
-            action:unexpected-eof-error
-            action:unexpected-byte-error))
-
-(define-public (guard:correct-first-byte? ctx byte)
+(define-public (correct-first-byte? ctx byte)
   (equal? byte 137))
-
-(define-public (guard:letter-P? ctx byte)
-  (equal? (integer->char byte) #\P))
-
-(define-public (guard:letter-N? ctx byte)
-  (equal? (integer->char byte) #\N))
-
-(define-public (guard:letter-G? ctx byte)
-  (equal? (integer->char byte) #\G))
-
-(define-public (guard:letter-LF? ctx byte)
-  (equal? (integer->char byte) #\linefeed))
-
-(define-public (guard:letter-CR? ctx byte)
-  (equal? (integer->char byte) #\return))
-
-(define-public (guard:letter-ctrl-z? ctx byte)
-  (equal? (integer->char byte) #\032))
-
 
-(define (action:wrong-first-byte-error ctx byte)
+(define (throw-wrong-first-byte-error ctx byte)
   (error "Wrong first byte" ctx byte))
 
-(define (action:unexpected-eof-error ctx byte)
+(define (throw-unexpected-eof-error ctx byte)
   (error "Unexpected end of file" ctx byte))
 
-(define (action:unexpected-byte-error ctx byte)
+(define (throw-unexpected-byte-error ctx byte)
   (error "Unexpected byte read" ctx byte))
