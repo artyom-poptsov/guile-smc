4ab22fd435cd8ff877e48536a3a7b0568c78e34b
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-04-23 21:38:53 +0300

    tests/compiler ("fsm-compile: target: standalone"): New test
    * tests/compiler.scm ("fsm-compile: target: standalone"): New test.

--- tests/compiler.scm
+++ tests/compiler.scm
@@ -36,6 +36,27 @@
       (lambda ()
         (fsm-compile fsm)))))
 
+(test-assert "fsm-compile: target: standalone"
+  (let ((fsm (make <fsm>
+               #:event-source char-context-event-source
+               #:transition-table `(((name . state-1)
+                                     (transitions
+                                      (,guard:#t ,action:no-op state-2)
+                                      (,guard:#t ,action:no-op state-1)))
+                                    ((name . state-2)
+                                     (transitions
+                                      (,guard:#t ,action:no-op state-1)
+                                      (,guard:#t ,action:no-op state-3)))
+                                    ((name . state-3)
+                                     (transitions
+                                      (,guard:#t ,action:no-op state-1)))))))
+    (with-output-to-string
+      (lambda ()
+        (fsm-compile fsm
+                     #:target 'guile-standalone
+                     #:modules-path (string-append (getenv "abs_top_srcdir")
+                                                   "/modules/smc/"))))))
+
 
 (define exit-status (test-runner-fail-count (test-runner-current)))
 
